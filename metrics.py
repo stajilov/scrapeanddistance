8508ee12e4076ad71530ce91ced2dbe1540cc44b
Vladimir Stajilov <stazhilov@gmail.com>
2017-08-28 19:36:36 +0300

    backup1

--- metrics.py
+++ metrics.py
@@ -3,6 +3,9 @@ import pandas as pd
 import codecs, difflib, Levenshtein, distance
 import re
 import string
+from collections import Counter
+
+import os.path
 
 def readCsvAsListOfDictionaries(fName):
     df = pd.read_csv(fName, error_bad_lines=False)
@@ -13,8 +16,20 @@ internalList = readCsvAsListOfDictionaries("institutions_new.csv")
 externalList = readCsvAsListOfDictionaries("3rdparty.csv")
 
 
+def majorityVoted(d):
+    lst = [d["LevName"], d["DiffName"], d["SorName"], d["JacName"]]
+    key, value = Counter(lst).most_common(1)[0]
+    print(key)
+    return key
+
+
 def writeDicToFile(dic, fileName):
-    
+    fileExists = os.path.exists(fileName)
+    with open(fileName, 'a+') as f:
+        w = csv.DictWriter(f, dic.keys())
+        if fileExists == False:
+            w.writeheader()
+        w.writerow(dic)
 
 
 def cleanString(s):
@@ -22,7 +37,6 @@ def cleanString(s):
 
 
 def calsulateDistances(st1, st2):
-    sr = row.lower().split("\t")
     diffl = difflib.SequenceMatcher(None, st1, st2).ratio()
     lev = Levenshtein.ratio(st1, st2)
     sor = 1 - distance.sorensen(st1, st2)
@@ -64,6 +78,15 @@ def evaluateMatches():
             if jac > jacValue:
                 jacValue = jac
                 jacName = targetName
+
+
+        summaryDic = dict(zip(('BaseName', 'DiffName', 'DiffValue', 'LevName','LevValue','SorName','SorValue','JacName','JacValue'), (baseName,diffName,diffValue,levName,levValue,sorName, sorValue, jacName, jacValue)))
+
+        print(summaryDic)
+        summaryDic["MajorityVoted"] = majorityVoted(summaryDic)
+        writeDicToFile(summaryDic, "matching_results.csv")
+
+
 
 
 
